57d199b097d846abbc427b1d466cbc39cb1ca08b
KLoyola <kevinloyola33@gmail.com>
2017-03-21 09:24:05 -0400

    3/22/17

--- CalendarViewController.swift
+++ CalendarViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CalendarViewController.swift
 //  StyleMe1
 //
 //  Created by Kevin Loyola on 3/20/17.
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CalendarViewController: UIViewController {
+    
+    @IBOutlet weak var CalendarBackButton: UIButton!
+    
+    @IBAction func CalendarBackButton(_ sender: Any) {
+        
+        print("Calendar Back to Home,  Button Pressed")
+        
+        self.performSegue(withIdentifier: "CalendarBacktoHome", sender: self)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
